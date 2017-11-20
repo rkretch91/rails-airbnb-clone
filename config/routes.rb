e50597071568d78ebb22dd2aa8659d34af53601e
Ryan Kretch <rkretch91@gmail.com>
2017-11-20 18:19:23 +0800

    routes adjusted

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :items do
+    resources :bookings, only: [ :new, :create, :edit, :update, :destroy ] do
+      resources :reviews, only: [ :new, :create ]
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
