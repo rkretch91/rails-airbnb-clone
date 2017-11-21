16757edb5dd40e3cdc18e7bbfd49e4c2be58b81d
Ryan Kretch <rkretch91@gmail.com>
2017-11-21 16:53:46 +0800

    added more to item controller

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  has_many :reviews, through: :bookings, dependent: :destroy
+  # has_many :reviews, through: :bookings, dependent: :destroy
 
   validates :name, presence: true
   validates :category, presence: true, inclusion: { in: ["bags", "jewellery", "fine watches", "belts", "ties", "glasses", "hats", "pocket squares", "cufflinks"], allow_nil: false}
