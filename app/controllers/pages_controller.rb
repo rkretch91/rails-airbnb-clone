68137d9adf534ae40dba11b6dd6b06aee7bb021a
Audrey Gourdji <agourdji@gmail.com>
2017-11-21 20:09:40 +0800

    Created cards for three categories. Clickable to item page. Still needs to be linked up with backend for all relevant information

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :items]
   def home
+    @items = Item.first(3)
+    #@shades = Item.find where category is shades
   end
 end
