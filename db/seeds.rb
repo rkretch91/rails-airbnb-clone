e5af8b5350981e33eaab5cc672ef2d2b9422515a
Sam Xia <pot365@gmail.com>
2017-11-21 14:37:34 +0800

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'Faker'
 
+Item.delete_all
+User.delete_all
 
 BRANDS = [
-  "A.P.C.","Acne Studios","Reem Acra","Aimé Leon Dore","Akris","Azzedine Alaïa","Reem Alasadi","Alexander McQueen (brand)","Alfred Dunhill Ltd.","Charlie Allen (designer)","Ally Capellino","Alpha Industries","Alter és Kiss","Anderson & Sheppard","André Laug","Andrés Sardá","Andrew Marc","Anna Valentine","Anne Fontaine (brand)","Aquascutum","Armani","Aspinal of London","Asprey","Audience by PETROU\MAN","Dominique Aurientis","H.J. Cave & Sons","Hackett London","Haerfest (fashion brand)","Kevan Hall","Halston","Tim Hamilton (designer)","Hansengross","Hardy Amies (fashion house)","Haus Alkire","Heavy Eco","Daniel Hechter","Helmut Lang (fashion brand)","Alexandre Herchcovitch","Hermès","Carolina Herrera (fashion designer)","Hield","Tommy Hilfiger (company)","Sherri Hill","Georges Hobeika","Hong Kong Tailors","House of Flora","H.Huntsman & Sons", "Agnès b.","Badgley Mischka","Balenciaga","Bally Shoe","Balmain (fashion house)","Neil Barrett (fashion designer)","Belle & Bunty","Bellville Sassoon","Belvest","Benson & Clegg","Berluti","Beulah London","Dirk Bikkembergs","Bill Blass Group","Manolo Blahnik","Ozwald Boateng","BodyMap","Boglioli","Bonia (fashion)","Bontoni","Borsalino","Hugo Boss","Bottega Veneta","Boué Soeurs","Tony Bowls","BP Studio","Veronique Branquinho","Malan Breton","Brioni (brand)","Thom Browne","Brunello Cucinelli (company)","Bulgari","Burberry","Nigel Cabourn","Cacharel","Calibre (Menswear)","Calvin Klein","Camilla and Marc","Caraceni","Pierre Cardin","Jane Carr (fashion designer)","Carrera y Carrera","Cartier (jeweler)","Dean and Dan Caten","Céline (brand)","Cerruti","Cerruti 1881","Hussein Chalayan","Chanel","Charvet Place Vendôme","List of Charvet customers","User:Chefmikesf/BVtest","The Chelsea Cobbler","Childrensalon","Chloé","Christian Dior SE","Matthew Christopher","Chrome Hearts","Clements Ribeiro","Colcci","Comme des Garçons","Common Projects","Jasper Conran","Cornelia James (company)","Corneliani","Costello Tagliapietra","Crolla","CuteCircuit","Cutler and Gross","Cyma Watches","Wendy Dagworthy","DAKS","Damani Dada","Damiani (jewelry company)","DAS Collection","Dege & Skinner","Alessandro Dell'Acqua","Ann Demeulemeester","Dior Homme","DKNY","Dolce & Gabbana","Drake's (haberdashers)","Duvelleroy","E. Thomas","Benoît-Pierre Emery","Enfants Riches Déprimés","Equipment (clothing brand)","Ermanno Scervino","Escada","Etro","Extè","Façonnable","Fendi","Gianfranco Ferré","Fiorucci","Tom Ford","Fossil Group","Franck Sorbier","Frey Wille","Frost French","Holly Fulton","Diane von Fürstenberg","Katie Gallagher","John Galliano","Jean-Paul Gaultier","Gieves & Hawkes","Giusto Manetti Battiloro","Givenchy","Golfino","Ana González (fashion designer)","Gottex","Goyard","Gravati","Great Greenland Furhouse","Gucci","Christopher Kane","Karen Kane","Kate Spade New York","Mary Katrantzou","Rabih Kayrouz","Kenzo (brand)","Khaadi","Kiton","Xander Kostroma","Kristall Smolensk","L.K.Bennett","Lachasse","Lacoste","Christian Lacroix","Karl Lagerfeld","Lagos (jewelry)","Derek Lam","Lancel (company)","Lanvin (company)","Lardini","Guy Laroche","Larusmiani","Judith Leiber","Léo Marciano","Monique Lhuillier","Phillip Lim"
+  "A.P.C.","Acne Studios","Reem Acra","Aimé Leon Dore","Akris","Azzedine Alaïa","Reem Alasadi","Alexander McQueen (brand)","Alfred Dunhill Ltd.","Charlie Allen (designer)","Ally Capellino","Alpha Industries","Alter és Kiss","Anderson & Sheppard","André Laug","Andrés Sardá","Andrew Marc","Anna Valentine","Anne Fontaine (brand)","Aquascutum","Armani","Aspinal of London","Asprey","Audience by PETROU","Dominique Aurientis","H.J. Cave & Sons","Hackett London","Haerfest (fashion brand)","Kevan Hall","Halston","Tim Hamilton (designer)","Hansengross","Hardy Amies (fashion house)","Haus Alkire","Heavy Eco","Daniel Hechter","Helmut Lang (fashion brand)","Alexandre Herchcovitch","Hermès","Carolina Herrera (fashion designer)","Hield","Tommy Hilfiger (company)","Sherri Hill","Georges Hobeika","Hong Kong Tailors","House of Flora","H.Huntsman & Sons", "Agnès b.","Badgley Mischka","Balenciaga","Bally Shoe","Balmain (fashion house)","Neil Barrett (fashion designer)","Belle & Bunty","Bellville Sassoon","Belvest","Benson & Clegg","Berluti","Beulah London","Dirk Bikkembergs","Bill Blass Group","Manolo Blahnik","Ozwald Boateng","BodyMap","Boglioli","Bonia (fashion)","Bontoni","Borsalino","Hugo Boss","Bottega Veneta","Boué Soeurs","Tony Bowls","BP Studio","Veronique Branquinho","Malan Breton","Brioni (brand)","Thom Browne","Brunello Cucinelli (company)","Bulgari","Burberry","Nigel Cabourn","Cacharel","Calibre (Menswear)","Calvin Klein","Camilla and Marc","Caraceni","Pierre Cardin","Jane Carr (fashion designer)","Carrera y Carrera","Cartier (jeweler)","Dean and Dan Caten","Céline (brand)","Cerruti","Cerruti 1881","Hussein Chalayan","Chanel","Charvet Place Vendôme","List of Charvet customers","User:Chefmikesf/BVtest","The Chelsea Cobbler","Childrensalon","Chloé","Christian Dior SE","Matthew Christopher","Chrome Hearts","Clements Ribeiro","Colcci","Comme des Garçons","Common Projects","Jasper Conran","Cornelia James (company)","Corneliani","Costello Tagliapietra","Crolla","CuteCircuit","Cutler and Gross","Cyma Watches","Wendy Dagworthy","DAKS","Damani Dada","Damiani (jewelry company)","DAS Collection","Dege & Skinner","Alessandro Dell'Acqua","Ann Demeulemeester","Dior Homme","DKNY","Dolce & Gabbana","Drake's (haberdashers)","Duvelleroy","E. Thomas","Benoît-Pierre Emery","Enfants Riches Déprimés","Equipment (clothing brand)","Ermanno Scervino","Escada","Etro","Extè","Façonnable","Fendi","Gianfranco Ferré","Fiorucci","Tom Ford","Fossil Group","Franck Sorbier","Frey Wille","Frost French","Holly Fulton","Diane von Fürstenberg","Katie Gallagher","John Galliano","Jean-Paul Gaultier","Gieves & Hawkes","Giusto Manetti Battiloro","Givenchy","Golfino","Ana González (fashion designer)","Gottex","Goyard","Gravati","Great Greenland Furhouse","Gucci","Christopher Kane","Karen Kane","Kate Spade New York","Mary Katrantzou","Rabih Kayrouz","Kenzo (brand)","Khaadi","Kiton","Xander Kostroma","Kristall Smolensk","L.K.Bennett","Lachasse","Lacoste","Christian Lacroix","Karl Lagerfeld","Lagos (jewelry)","Derek Lam","Lancel (company)","Lanvin (company)","Lardini","Guy Laroche","Larusmiani","Judith Leiber","Léo Marciano","Monique Lhuillier","Phillip Lim"
 ]
 
 
 CATEGORY = [
-  “bags”, “jewellery”, “fine watches”, “belts”, “ties”, “glasses”, “hats”, “pocket squares”, “cufflinks”
+  "bags", "jewellery", "fine watches", "belts", "ties", "glasses", "hats", "pocket squares", "cufflinks"
 ]
 CONDITION = [
-  “perfect”, “very good”, “good”, “worn well”
+  "perfect", "very good", "good", "worn well"
 ]
 
 
+DESCRIPTIOPN = [
+  "an A-line skirt fits closely around the waist and is slightly wider at the bottom","backless adjective","a backless dress does not cover a woman’s back and shoulders","baggy adjective","baggy clothes are very loose on your body","best adjective","used for referring to your nicest or most expensive clothes that you wear on special,occasions",
+"boot-cut adjective","boot-cut trousers become slightly wider at the bottom so that they fit easily over,boots",
+"brief adjective","not covering much of your body","button-down adjective","a button-down shirt has the ends of the collar fastened to the shirt with buttons","button-through adjective","british a button-through skirt or dress has buttons that go from the top to the,bottom",
+"casual adjective","casual clothes are comfortable, and suitable for wearing in informal situations","clingy adjective","informal fitting in a way that shows the shape of your body"
+]
+
+puts 'Creating fake data...'
 
 # user
 # email first_name last_name address phone_number password
@@ -49,17 +52,18 @@ user_create(20)
 # name category description condition brand price user_id
 
 def item_create(number_of_items)
-  number_of_users.times do
+  number_of_items.times do
 
-    user_params = {
+    item_params = {
+      name:Faker::StarWars.droid,
       category: CATEGORY.sample,
-      description: Faker::Name.first_name,
-      condition: CONDITION.sample
+      description: DESCRIPTIOPN.sample,
+      condition: CONDITION.sample,
       brand: BRANDS.sample,
-      price: "#{Faker::Number.between(200, 2000)}"
-      user_id: "123123"
+      price: Faker::Number.between(200, 2000),
+      user_id: User.all.sample.id
     }
-    Item.create(user_params)
+    Item.create(item_params)
   end
 end
 
