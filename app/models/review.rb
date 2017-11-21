4cebf4553f97906f56cb5dcc8731f7e1957260e5
Ryan Kretch <rkretch91@gmail.com>
2017-11-21 10:58:27 +0800

    validations and dependencies added

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :booking
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :user_rating, presence: true, inclusion: { in: [1,2,3,4,5]}
+  validates :item_rating, presence: true, inclusion: { in: [1,2,3,4,5]}
+
 end
