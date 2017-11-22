2c9ad29cdcb6040946366b1e39f449309323399d
Ryan Kretch <rkretch91@gmail.com>
2017-11-22 11:07:07 +0800

    made changes on page cards backend

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :items]
   def home
     # @item = Item.first(3)
-    @ties = Item.find_by(category: :ties)
-    @belts = Item.find_by(category: :belts)
-    @shades = Item.find_by(category: :shades)
+    @ties = Item.where(category: 'ties').order(created_at: :desc).limit(3)
+    @belts = Item.where(category: 'belts').order(created_at: :desc).limit(3)
+    @shades = Item.where(category: 'shades').order(created_at: :desc).limit(3)
   end
 end
