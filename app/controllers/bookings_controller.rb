7c19a14a0a37c7aa7e097a29d8a2387b9d586e94
Ryan Kretch <rkretch91@gmail.com>
2017-11-23 13:47:51 +0800

    continuing to edit

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -27,6 +27,9 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to dashboard_path
   end
 
   private
